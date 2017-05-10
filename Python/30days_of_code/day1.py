f0978f6df90c0a55fd6a51026f06815b9209130d
Francois Czarny <frczarny@gmail.com>
2017-05-10 23:43:19 +0200

    First "problem" on the 30days of code on HR

---
+++ Python/30days_of_code/day1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding : utf-8 -*-
+
+"""
+Task
+Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
+
+Declare  variables: one of type int, one of type double, and one of type String.
+Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
+Use the  operator to perform the following operations: 
+Print the sum of  plus your int variable on a new line.
+Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+Concatenate  with the string you read as input and print the result on a new line.
+
+"""
+
+# Declare second integer, double, and String variables.
+i2 = int()
+d2 = float()
+s2 = str()
+# Read and save an integer, double, and String to your variables.
+i2 = int(raw_input())
+d2 = float(raw_input())
+s2 = str(raw_input())
+# Print the sum of both integer variables on a new line.
+print i+i2
+# Print the sum of the double variables on a new line.
+print d+d2
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s+s2
