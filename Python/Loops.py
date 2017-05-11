3ef89f3448b09b13d6f7b927d8b2a00762165729
Francois Czarny <frczarny@gmail.com>
2017-05-12 00:03:17 +0200

    Add solution to Loops problem of HR website

---
+++ Python/Loops.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding : utf-8 -*-
+
+"""
+Task 
+Read an integer N. For all non-negative integers i<N, print i^2 . See the sample for details.
+"""
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(n):
+        print i**2
